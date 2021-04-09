3247ee01b22fc54c6f0b0db6a3f5a0f92f0a86ee
cleverson <http://42715@alm.tjmt.jus.br>
2021-04-09 19:08:48 -0400

    Ajuste docker compose

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,10 +6,10 @@ echo "--- Variáveis de ambiente ---"
 printenv | sort
 echo "-----------------------------"
 
-envsubst < "/usr/share/nginx/html/assets/config/config.json" > "/usr/share/nginx/html/assets/config/config-docker.json"
+envsubst < "/usr/share/nginx/html/assets/config/config-docker.json" > "/usr/share/nginx/html/assets/config/config.json"
 
 echo "--- Configurações das variáveis de ambiente utilizadas ---"
-cat /usr/share/nginx/html/assets/config/config-docker.json
+cat /usr/share/nginx/html/assets/config/config.json
 echo "----------------------------------------------------------"
 
 nginx -g "daemon off;"
